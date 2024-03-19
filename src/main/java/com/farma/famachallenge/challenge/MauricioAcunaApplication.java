da4800100006b1266ea814979a0e3fc88a875214
maundroid <maundroid@gmail.com>
2024-03-19 23:44:15 +0100

    first up

---
+++ src/main/java/com/farma/famachallenge/challenge/MauricioAcunaApplication.java
@@ -0,0 +1,13 @@
+package com.farma.famachallenge.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MauricioAcunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MauricioAcunaApplication.class, args);
+	}
+
+}
